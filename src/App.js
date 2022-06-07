bc8bbd3bb19b3771edc4319d56927fc943481038
usa3amustafa <usa3a3ustafa@outlook.com>
2022-06-07 22:22:23 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Topbar from './components/Topbar'
-import Sidebar from './components/Sidebar'
 import Dashboard from './components/dashboard/Dashboard'
 import Form from './components/Form'
 
@@ -8,16 +7,8 @@ const App = () => {
   return (
     <>
       <Topbar />
-
-      <main>
-        <div className='container main'>
-          <Sidebar />
-          <div className='dashboard'>
-            {/* <Form /> */}
-            <Dashboard />
-          </div>
-        </div>
-      </main>
+      {/* <Form /> */}
+      <Dashboard />
     </>
   )
 }
--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -2,14 +2,22 @@ import React from 'react'
 import Dropdown from './Dropdown'
 import dData from '../../dashboardData'
 import Carousel from './Carousel'
+import Sidebar from '../../components/Sidebar'
 
 const Dashboard = () => {
   return (
     <>
-      <Carousel />
-      {dData.map(item => {
-        return <Dropdown key={item.id} dropdownItem={item} />
-      })}
+      <main>
+        <div className='main'>
+          <Sidebar />
+          <div className='dashboard'>
+            <Carousel />
+            {dData.map(item => {
+              return <Dropdown key={item.id} dropdownItem={item} />
+            })}
+          </div>
+        </div>
+      </main>
     </>
   )
 }
